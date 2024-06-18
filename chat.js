ba03b87fc56f2e5104676cc54c94d3279947e4c7
allydoescode <156075325+allydoescode@users.noreply.github.com>
2024-06-18 17:44:17 +0100

    tmi.js and connecting to chat

---
+++ chat.js
@@ -0,0 +1,46 @@
+const CLIENT_ID     = ""
+const CLIENT_SECRET = ""
+const REFRESH_TOKEN = ""
+
+const USERNAME      = ""
+const CHANNEL       = ""
+
+function getAccessToken() {
+    xhr = new XMLHttpRequest()
+    xhr.open("POST", "https://id.twitch.tv/oauth2/token", false)
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xhr.send(`client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${REFRESH_TOKEN}`)
+
+    if (xhr.status === 200) return JSON.parse(xhr.response).access_token
+}
+
+const ACCESS_TOKEN = getAccessToken()
+
+function getTwitchID() {
+    xhr = new XMLHttpRequest()
+    xhr.open("GET", `https://api.twitch.tv/helix/users?login=${CHANNEL}`, false)
+    xhr.setRequestHeader("Authorization", `Bearer ${ACCESS_TOKEN}`)
+    xhr.setRequestHeader("Client-Id", CLIENT_ID)
+    xhr.send(null)
+
+    if (xhr.status === 200) return JSON.parse(xhr.response).data[0].id
+}
+
+const client = new tmi.Client({
+    options: { debug: true },
+    connection: {
+        reconnect: true,
+        secure: true,
+    },
+    identity: {
+        username: USERNAME,
+        password: `oauth:${ACCESS_TOKEN}`
+    },
+    channels: [ CHANNEL ]
+})
+
+client.connect()
+
+client.on("message", (channel, tags, message, self) => {
+    console.log(`${tags.username}: ${message}`)
+})
